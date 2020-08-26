bc7d962ee7dd69e9008f326c209d515c66dc0005
leo18731358 <pzhang2015@163.com>
2020-08-26 23:00:47 +0800

    提交版本1.0.8.2602

--- LogHelper.cpp
+++ LogHelper.cpp
@@ -1,5 +1,9 @@
 #include "LogHelper.h"
 
+#include <QDateTime>
+#include <Qdir>
+
+
 LogHelper::LogHelper(QObject *parent)
 	: QObject(parent)
 {
@@ -12,10 +16,31 @@ LogHelper::~LogHelper()
 
 QString LogHelper::CreateDayFile(QString pathBase)
 {
+	try
+	{
+		//获取当前日期
+		QDateTime dt = QDateTime::currentDateTime();
+		//用时间命名
+		QString Year = dt.date().year();
+		QString Month = dt.date().month();
+		QString Day = dt.date().day();
 
+		QString path = pathBase + Year + "-" + Month + "-" + Day + "\\";
 
+		//判断是否存在目录夹
+		QDir dir;
 
-	return "";
+		if (!dir.exists(path))
+		{
+			dir.mkdir(path);
+		}
+		return path;
+	}
+	catch (const std::exception&)
+	{
+		return "";
+	}
+
 }
 
 QString LogHelper::CreateHourFile(QString pathBase)
